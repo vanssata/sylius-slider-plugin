9ade7fd7c8f3dbfd7d99820bcc2219577ec8f28a
vansa <ivan.kakurov@gmail.com>
2026-02-15 16:17:25 +0200

    Fix composer version

--- src/Twig/SliderExtension.php
+++ src/Twig/SliderExtension.php
@@ -4,41 +4,36 @@ declare(strict_types=1);
 
 namespace Vanssa\SyliusSliderPlugin\Twig;
 
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Attribute\AsTwigFunction;
+use Twig\Environment;
+use Twig\Markup;
 use Vanssa\SyliusSliderPlugin\Repository\SlideRepository;
 use Vanssa\SyliusSliderPlugin\Repository\SliderRepository;
-use Twig\Environment;
-use Twig\Extension\AbstractExtension;
-use Twig\Markup;
-use Twig\TwigFunction;
 
-final class SliderExtension extends AbstractExtension
+final readonly class SliderExtension
 {
     public function __construct(
-        private readonly SliderRepository $sliderRepository,
-        private readonly SlideRepository $slideRepository,
-        private readonly Environment $twig,
+        private SliderRepository $sliderRepository,
+        private SlideRepository  $slideRepository,
+        private RouterInterface  $router,
+        private Environment      $twig,
     ) {
     }
 
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('sylius_slider_by_code', $this->findSliderByCode(...)),
-            new TwigFunction('sylius_slide_by_code', $this->findSlideByCode(...)),
-            new TwigFunction('sylius_slider_render_content', $this->renderContent(...), ['is_safe' => ['html']]),
-        ];
-    }
-
+    #[AsTwigFunction('sylius_slider_by_code')]
     public function findSliderByCode(string $code): mixed
     {
         return $this->sliderRepository->findEnabledOneByCode($code);
     }
 
+    #[AsTwigFunction('sylius_slide_by_code')]
     public function findSlideByCode(string $code): mixed
     {
         return $this->slideRepository->findEnabledOneByCode($code);
     }
 
+    #[AsTwigFunction('sylius_slider_render_content', isSafe: ['html'])]
     public function renderContent(?string $content): Markup
     {
         $content ??= '';
@@ -51,5 +46,11 @@ final class SliderExtension extends AbstractExtension
         $template = $this->twig->createTemplate('{{ content|monsieurbiz_richeditor_render_field }}');
 
         return new Markup($template->render(['content' => $content]), 'UTF-8');
+    }
+
+    #[AsTwigFunction('vanssa_route_exists')]
+    public function routeExists(string $routeName): bool
+    {
+        return null !== $this->router->getRouteCollection()->get($routeName);
     }
 }
